3dae0ed838436a8ed866f850b9a2988654da5614
Jack Mousseau <jackmousseau@gmail.com>
2016-08-23 19:24:12 -0400

    Add write_slurm_script SlurmBashScript method

--- R/slurm-bash-script.r
+++ R/slurm-bash-script.r
@@ -2,4 +2,40 @@
 #'
 #' Generates the necessary bash script to submit through
 #' the `sbatch` command.
-SlurmBashScript <- R6::R6Class("SlurmBashScript")
+SlurmBashScript <- R6::R6Class("SlurmBashScript",
+    public = list(
+        initialize = function(container, main_file, copy_back = "*") {
+            private$write_slurm_script(container$dir)
+        }
+    ),
+    private = list(
+        write_slurm_script = function(dir) {
+            contents <- "
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=12
+#SBATCH --time=0:10:00
+#SBATCH --mem=16g
+#SBATCH -o R_job.o%j
+
+
+# copy necessary files over
+cp -r ./source ./input ./.objects $PFSDIR
+cd $PFSDIR
+
+module load hpc-ods
+module load pandoc
+
+R CMD BATCH $1
+
+for i in ${@:2}
+do
+cp -r $i $SLURM_SUBMIT_DIR/output
+done
+
+cp -r './$1out' $SLURM_SUBMIT_DIR/output"
+
+            write(contents, file = paste(dir, ".static.slurm", sep = "/"))
+        }
+    )
+)
