7e41faf8e18adcf2a368e689a1d3bbced5243f29
Jack Mousseau <jackmousseau@gmail.com>
2016-08-26 15:28:04 -0400

    Add submit_jobs function for submission using R

--- R/slurm-job.r
+++ R/slurm-job.r
@@ -111,3 +111,32 @@ SlurmJob <- R6::R6Class("SlurmJob",
         }
     )
 )
+
+
+#' Submit one or more slurm jobs.
+#'
+#' This function will submit your slurm job given the path
+#' to a slurm container.
+#'
+#' @param jobs The \code{job_<alphanumeric>/} directories for
+#' the slurm container. May also
+#'
+#' @export
+submit_jobs <- function(jobs) {
+    wd <- getwd()
+
+    for (dir in jobs) {
+        tryCatch({
+            setwd(dir)
+            system("sh submit.sh")
+        }, error = function(e) {
+            setwd(wd)
+            stop(e)
+        })
+
+    }
+
+    setwd(wd)
+}
+
+
