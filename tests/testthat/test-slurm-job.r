6abaf9b06fc96f8fae1ed463c365167442dbaa18
Jack Mousseau <jackmousseau@gmail.com>
2016-08-26 07:42:07 -0400

    Remove main_file test

--- tests/testthat/test-slurm-job.r
+++ tests/testthat/test-slurm-job.r
@@ -2,7 +2,5 @@ context("SlurmJob")
 
 
 test_that("SlurmJob initializer sets main_file property.",  {
-    sj <- SlurmJob$new("main.R")
-    expect_equal(sj$main_file, "main.R")
     expect_error(SlurmJob$new())
 })
