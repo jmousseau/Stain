08bb83192d9b9f0a14dee9eeaa33f9a0ec52e6b1
Jack Mousseau <jackmousseau@gmail.com>
2016-08-26 08:01:47 -0400

    Add test-sbatch.r

---
+++ tests/testthat/test-sbatch.r
@@ -0,0 +1,30 @@
+context("sbatch")
+
+
+test_that("All options are formated correctly", {
+    expect_equal(sbatch_opts$begin("00:00:01"), "--begin=00:00:01")
+    expect_equal(sbatch_opts$cpus_per_task(12), "--cpus-per-task=12")
+    expect_equal(sbatch_opts$mail_user("user@address"),
+                 "--mail-user=user@address")
+    expect_equal(sbatch_opts$memory(1200), "--mem=1200")
+    expect_equal(sbatch_opts$memory("16g"), "--mem=16g")
+    expect_equal(sbatch_opts$nodes(1), "--nodes=1")
+    expect_equal(sbatch_opts$ouput("file.txt"), "--ouput=file.txt")
+    expect_equal(sbatch_opts$time("00:00:01"), "--time=00:00:01")
+})
+
+test_that("sbatch_opt creates a new key-value option.", {
+    expect_equal(sbatch_opt("key")("value"), "--key=value")
+})
+
+test_that("Options equality is base on option keys.", {
+    a <- sbatch_opt("a")("true")
+    b <- sbatch_opt("b")("false")
+    a_ <- sbatch_opt("a")("false")
+
+    expect_true(sbatch_opts_equal(a, a_))
+    expect_false(sbatch_opts_equal(a, b))
+
+    expect_equal(sbatch_opts_insert(a_, c(a, b)), c(a_, b))
+    expect_equal(sbatch_opts_insert(a, c(b)), c(b, a))
+})
