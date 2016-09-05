edb1976c657bc461972576558ea3a40bb4096c87
Jack Mousseau <jackmousseau@gmail.com>
2016-09-05 19:25:43 -0400

    Have stain_new return SlurmContainer instead of SJ

--- R/slurm-job.r
+++ R/slurm-job.r
@@ -1,60 +1,3 @@
-#' SlurmJob R6 object.
-#'
-#' A wrapper around SlurmContainer objects.
-SlurmJob <- R6::R6Class("SlurmJob",
-    public = list(
-        params = list(),
-        main_file = NULL,
-        data_files = list(),
-        source_files = list(),
-        initialize = function(main_file, container_location = ".",
-                              source_files = list(),
-                              settings = SlurmSettings$new()) {
-            if (!missing(main_file)) {
-                self$main_file <- main_file
-
-                self$source_files <- source_files
-
-                private$base_dir <- container_location
-                globals <- find_globals(c(main_file, source_files))
-                self$params <- globals
-                private$globals <- globals
-                private$settings <- settings
-            } else {
-                stop("A file containing a main() function must be provided.")
-            }
-        },
-        create = function(allow_creation_without_data_files = FALSE) {
-
-            if (!allow_creation_without_data_files && length(self$data_files) == 0) {
-                stop("Attempting to create slurm job without `data_files`. Pass TRUE to `create` to override.")
-            }
-
-            container <- SlurmContainer$new(private$base_dir)
-
-            tryCatch({
-                for (name in names(self$params)) {
-                    container$add_object(name, self$params[[name]])
-                }
-
-                container$add_sources(c(self$source_files, self$main_file))
-                container$add_data(self$data_files)
-            }, error = function(e) {
-                container$delete(TRUE)
-                stop(e)
-            })
-
-            script <- SlurmBashScript$new(container, private$settings)
-        }
-    ),
-    private = list(
-        globals = list(),
-        base_dir = ".",
-        settings = NA
-    )
-)
-
-
 #' Submit one or more slurm jobs.
 #'
 #' This function will submit your slurm job given the path
@@ -80,5 +23,3 @@ submit_jobs <- function(jobs) {
 
     setwd(wd)
 }
-
-
--- R/stain.r
+++ R/stain.r
@@ -1,20 +1,26 @@
 #' Create a new slurm container.
 #'
-#' IMPORTANT: To call this function use \code{Stain$new}.
-#'
-#' @param main An R source file containing a \code{main()} function.
-#'
 #' @param location The directory where to create the slurm container. The
 #' default location is the current working directory.
 #'
-#' @param source_files R source files other than \code{main}.
+#' @param source_files R source files.
 #'
 #' @param settings A \code{SlurmSettings} object.
 #'
-#' @return A \code{SlurmJob} object.
+#' @return A \code{SlurmContainer} object.
 #'
 #' @export
-stain_new <- function(main, location = ".", source_files = list(),
+stain_new <- function(location = ".", source_files = list(),
                       settings = SlurmSettings$new()) {
-    return(SlurmJob$new(main, location, source_files, settings))
+
+    stain <- SlurmContainer$new(location, settings)
+
+    tryCatch({
+        stain$add_sources(source_files)
+    }, error = function(e) {
+        stain$delete(TRUE)
+        stop(e)
+    })
+
+    return(stain)
 }
