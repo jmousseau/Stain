ce0489a4a3414ac9d1ac17da2d435b27618d9c57
Jack Mousseau <jackmousseau@gmail.com>
2016-09-05 16:19:23 -0400

    Make add_file & remove_file plural
    On failure, the copy or removal will be aborted meaning not one
    of the files will be copied or removed.

--- R/slurm-container.r
+++ R/slurm-container.r
@@ -6,6 +6,7 @@
 SlurmContainer <- R6::R6Class("SlurmContainer",
     public = list(
         dir = NULL,
+        globals = list(),
         initialize = function(dir = ".") {
             self$dir <- dir
 
@@ -47,20 +48,20 @@ SlurmContainer <- R6::R6Class("SlurmContainer",
                 stop("Object must have an non NA value.")
             }
         },
-        remove_object = function(name) {
-            private$remove_file(paste0(name, ".RData"), "objects")
+        remove_objects = function(names) {
+            private$remove_files(paste0(names, ".RData"), "objects")
         },
-        add_source = function(file) {
-            private$add_file(file, "sources")
+        add_sources = function(files) {
+            private$add_files(files, "sources")
         },
-        remove_source = function(basename) {
-            private$remove_file(file, "sources")
+        remove_sources = function(basenames) {
+            private$remove_files(basenames, "sources")
         },
-        add_data = function(file) {
-            private$add_file(file, "data")
+        add_data = function(files) {
+            private$add_files(files, "data")
         },
-        remove_data = function(basename) {
-            private$remove_file(file, "data")
+        remove_data = function(basenames) {
+            private$remove_files(basenames, "data")
         },
         get_files = function() {
             return(list(
@@ -71,15 +72,26 @@ SlurmContainer <- R6::R6Class("SlurmContainer",
         }
     ),
     private = list(
-        add_file = function(file, stain_sub_dir) {
-            if (!file.exists(file)) { stop("File does not exist.") }
+        add_files = function(files, stain_sub_dir) {
             dir <- paste(self$dir, ".stain", stain_sub_dir, sep = "/")
-            file.copy(file, dir, recursive = TRUE)
+
+            for (file in files) {
+                if (!file.exists(file)) { stop("File does not exist. Copy aborted.") }
+            }
+
+            for (file in files) {
+                file.copy(file, dir, recursive = TRUE)
+            }
         },
-        remove_file = function(file, stain_sub_dir) {
-            if (!file.exists(file)) { stop("File does not exist.") }
-            file <- paste(self$dir, ".stain", stain_sub_dir, file, sep = "/")
-            file.remove(file)
+        remove_files = function(files, stain_sub_dir) {
+            for (file in files) {
+                if (!file.exists(file)) { stop("File does not exist. Removal aborted.") }
+            }
+
+            for (file in files) {
+                file <- paste(self$dir, ".stain", stain_sub_dir, file, sep = "/")
+                file.remove(file)
+            }
         },
         rand_alphanumeric = function(len = 3) {
             population <- c(rep(0:9, each = 5), LETTERS, letters)
