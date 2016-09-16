2288444c722b737da6149fc3cbf6c7f6bd60d9d7
Jack Mousseau <jackmousseau@gmail.com>
2016-09-16 12:55:12 -0400

    Add stain_ssh_key_gen function

--- R/stain-ssh.r
+++ R/stain-ssh.r
@@ -5,3 +5,17 @@
 stain_ssh_key_exists <- function() {
     return("stain_rsa" %in% list.files("~/.ssh/"))
 }
+
+
+#' Generate a Stain ssh key.
+#'
+#' A 4096 bit key will be generated and stored in \code{~/.ssh/} with the name
+#' \code{stain_rsa}.
+#'
+#' @param overwrite Should an existing Stain ssh key be overwritten. Default
+#' value is FALSE.
+stain_ssh_key_gen <- function(overwrite = FALSE) {
+    if (overwrite | !(overwrite | stain_ssh_key_exists())) {
+        system("ssh-keygen -b 4096 -f ~/.ssh/stain_rsa")
+    }
+}
