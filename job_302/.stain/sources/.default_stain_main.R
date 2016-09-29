a477d7bddfb5db10133f33002a75ce6d2ba945b2
Jack Mousseau <jackmousseau@gmail.com>
2016-09-29 13:17:48 -0400

    Remove accidental job dir & ignore all future jobs

--- job_302/.stain/sources/.default_stain_main.R
+++
@@ -1,7 +0,0 @@
-
-
-
-sapply(list.files('./.stain/sources', full.names = TRUE)[!(list.files('./.stain/sources')) %in% '.default_stain_main.R' ], source)
-sapply(list.files('./.stain/objects', full.names = TRUE),
-                             function(file) { load(file, env = .GlobalEnv) })
-main()
--- job_302/.stain/sources/testMain.R
+++
@@ -1,5 +0,0 @@
-
-main <- function() {
-    data <- data.table::fread(paste("./data", my_file_name, sep = "/"))
-    write.csv(data, csv_filename(my_file_name) , row.names = FALSE)
-}
